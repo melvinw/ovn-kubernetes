083054e340ffaf1ce440653a2311087591f3209f
Melvin Walls <mwalls67@gmail.com>
2015-12-25 21:20:21 -0800

    overlay: simplify setup and teardown by using ovn-docker

--- ovn-k8-overlay.py
+++ ovn-k8-overlay.py
@@ -74,6 +74,21 @@ def get_annotations(pod_name, namespace):
                 return None
 
 
+def get_pod(pod_name, namespace):
+    api_server = ovs_vsctl("--if-exists get open_vswitch . "
+                           "external-ids:api_server").strip('"')
+    if not api_server:
+        return None
+
+    url = "http://%s/api/v1/namepsaces/%s/pods/%s" % (api_server, namespace,
+            pod_name)
+    response = requests.get(url)
+    if response:
+        return response.json()
+    else:
+        return None
+
+
 def associate_security_group(lport_id, security_group_id):
     pass
 
@@ -92,163 +107,12 @@ def plugin_setup(args):
     pod_name = args.k8_args[1]
     container_id = args.k8_args[2]
 
-    get_ovn_remote()
-
-    client = Client(base_url='unix://var/run/docker.sock')
-    try:
-        inspect = client.inspect_container(container_id)
-        pid = inspect["State"]["Pid"]
-        ip_address = inspect["NetworkSettings"]["IPAddress"]
-        netmask = inspect["NetworkSettings"]["IPPrefixLen"]
-        mac = inspect["NetworkSettings"]["MacAddress"]
-        gateway_ip = inspect["NetworkSettings"]["Gateway"]
-    except Exception as e:
-        error = "failed to get container pid and ip address (%s)" % (str(e))
-        sys.exit(error)
-
-    if not pid:
-        sys.exit("failed to fetch the pid")
-
-    netns_dst = "/var/run/netns/%s" % (pid)
-    if not os.path.isfile(netns_dst):
-        netns_src = "/proc/%s/ns/net" % (pid)
-        command = "ln -s %s %s" % (netns_src, netns_dst)
-
-        try:
-            call_popen(shlex.split(command))
-        except Exception as e:
-            error = "failed to create the netns link"
-            sys.exit(error)
-
-    # Delete the existing veth pair
-    command = "ip netns exec %s ip link del eth0" % (pid)
+    command = "docker network connect di %s" % (conatiner_id)
     try:
         call_popen(shlex.split(command))
     except Exception as e:
-        error = "failed to delete the default veth pair"
+        error = "Failed to connect to lswitch (%s)" % (str(e),)
         sys.stderr.write(error)
-
-    veth_outside = container_id[0:15]
-    veth_inside = container_id[0:13] + "_c"
-    command = "ip link add %s type veth peer name %s" \
-              % (veth_outside, veth_inside)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to create veth pair (%s)" % (str(e))
-        sys.exit(error)
-
-    # Up the outer interface
-    command = "ip link set %s up" % veth_outside
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to admin up veth_outside (%s)" % (str(e))
-        sys.exit(error)
-
-    # Move the inner veth inside the container
-    command = "ip link set %s netns %s" % (veth_inside, pid)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to move veth inside (%s)" % (str(e))
-        sys.exit(error)
-
-    # Change the name of veth_inside to eth0
-    command = "ip netns exec %s ip link set dev %s name eth0" \
-              % (pid, veth_inside)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to change name to eth0 (%s)" % (str(e))
-        sys.exit(error)
-
-    # Up the inner interface
-    command = "ip netns exec %s ip link set eth0 up" % (pid)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to admin up veth_inside (%s)" % (str(e))
-        sys.exit(error)
-
-    # Set the mtu to handle tunnels
-    command = "ip netns exec %s ip link set dev eth0 mtu %s" \
-              % (pid, 1450)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to set mtu (%s)" % (str(e))
-        sys.exit(error)
-
-    # Set the ip address
-    command = "ip netns exec %s ip addr add %s/%s dev eth0" \
-              % (pid, ip_address, netmask)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to set ip address (%s)" % (str(e))
-        sys.exit(error)
-
-    # Set the mac address
-    command = "ip netns exec %s ip link set dev eth0 address %s" % (pid, mac)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to set mac address (%s)" % (str(e))
-        sys.exit(error)
-
-    # Set the gateway
-    command = "ip netns exec %s ip route add default via %s" \
-              % (pid, gateway_ip)
-    try:
-        call_popen(shlex.split(command))
-    except Exception as e:
-        error = "Failed to set gateway (%s)" % (str(e))
-        sys.exit(error)
-
-    # Get the logical switch
-    try:
-        lswitch = ovs_vsctl("--if-exists get open_vswitch . "
-                            "external_ids:lswitch").strip('"')
-        if not lswitch:
-            error = "No lswitch created for this host"
-            sys.exit(error)
-    except Exception as e:
-        error = "Failed to get external_ids:lswitch  (%s)" % (str(e))
-        sys.exit(error)
-
-    # Create a logical port
-    try:
-        ovn_nbctl("lport-add %s %s" % (lswitch, container_id))
-    except Exception as e:
-        error = "lport-add %s" % (str(e))
-        sys.exit(error)
-
-    # Set the ip address and mac address
-    try:
-        ovn_nbctl("lport-set-addresses %s \"%s %s\""
-                  % (container_id, mac, ip_address))
-    except Exception as e:
-        error = "lport-set-addresses %s" % (str(e))
-        sys.exit(error)
-
-    # Add the port to a OVS bridge and set the vlan
-    try:
-        ovs_vsctl("add-port %s %s -- set interface %s "
-                  "external_ids:attached_mac=%s external_ids:iface-id=%s "
-                  "external_ids:ip_address=%s"
-                  % (OVN_BRIDGE, veth_outside, veth_outside, mac,
-                     container_id, ip_address))
-    except Exception as e:
-        ovn_nbctl("lport-del %s" % container_id)
-        error = "failed to create a OVS port. (%s)" % (str(e))
-        sys.exit(error)
-
-    annotations = get_annotations(ns, pod_name)
-    if annotations:
-        security_group = annotations.get("security-group", "")
-        if security_group:
-            associate_security_group(lport, security_group)
 
 
 def plugin_status(args):
@@ -274,32 +138,11 @@ def plugin_teardown(args):
     pod_name = args.k8_args[1]
     container_id = args.k8_args[2]
 
-    get_ovn_remote()
-
-    veth_outside = container_id[0:15]
-    command = "ip link delete %s" % (veth_outside)
+    command = "docker network disconnect di %s" % (conatiner_id)
     try:
         call_popen(shlex.split(command))
     except Exception as e:
-        error = "Failed to delete veth_outside (%s)" % (str(e))
-        sys.stderr.write(error)
-
-    annotations = get_annotations(ns, pod_name)
-    if annotations:
-        security_group = annotations.get("security-group", "")
-        if security_group:
-            disassociate_security_group(container_id)
-
-    try:
-        ovn_nbctl("lport-del %s" % container_id)
-    except Exception as e:
-        error = "failed to delete logical port (%s)" % (str(e))
-        sys.stderr.write(error)
-
-    try:
-        ovs_vsctl("del-port %s" % (veth_outside))
-    except Exception as e:
-        error = "failed to delete OVS port (%s)" % (veth_outside)
+        error = "Failed to connect to lswitch (%s)" % (str(e),)
         sys.stderr.write(error)
 
 
